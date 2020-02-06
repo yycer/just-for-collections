e16c933729cc35132f15f25282241b200555981b
yycer <15900617310@163.com>
2020-02-06 10:57:37 +0800

    commit project.

---
+++ src/test/java/com/frankie/fun/FunApplicationTests.java
@@ -0,0 +1,14 @@
+package com.frankie.fun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println(1);
+    }
+
+}
